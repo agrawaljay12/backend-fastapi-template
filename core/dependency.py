5a676267a1d2cb4e179cee5dc5cc9743205ea626
agrawaljay12 <agrawaljay247@gmail.com>
2026-01-17 15:30:54 +0530

    enhancement the test cases of a user auth

--- core/dependency.py
+++ core/dependency.py
@@ -1,3 +1,4 @@
+from typing import List
 from fastapi import Depends,HTTPException,status
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
--- test_case/test_user.py
+++ test_case/test_user.py
@@ -1,21 +1,24 @@
+import uuid
 from fastapi.testclient import TestClient
-from main import app
+from app.main import app
+
+# # from models.users import User
+# import routes.version.v1.user_routes as user_router
 
 client = TestClient(app)
 
 # test case for create user endpoint
 def test_create_user_success():
-
-    data ={
+    data = {
         "name": "Test User",
-        "email": "test@gmail.com",
+        "email": f"test_{uuid.uuid4()}@gmail.com",
         "password": "Test@1234"
     }
 
-    response = client.post("/api/v1/users/create",json=data)
-    
+    response = client.post("/api/v1/users/create", json=data)
+
     assert response.status_code == 200
-    assert response.json()["message"] == "User created successfully"
+
 
 # test case for Invalid password format ,email format ,Invalid name format , Invalid email already exists
 def test_create_user_invalid_email_format():
@@ -56,29 +59,44 @@ def test_create_user_invalid_password_format():
 
     assert response.status_code == 400
     assert response.json()["detail"] == "Invalid password format"
-
+  
 #  test case for login user endpoint
 def test_login_success():
-    payload = {
+
+    # STEP 1: Create user
+    create_payload = {
+        "name": "Jay Test",
         "email": "jay@test.com",
         "password": "Jay@12345"
     }
+    client.post("/api/v1/users/create", json=create_payload)
 
-    response = client.post("/users/login", json=payload)
+    # STEP 2: Login user
+    login_payload = {
+        "email": "jay@test.com",
+        "password": "Jay@12345"
+    }
+    response = client.post("/api/v1/users/login", json=login_payload)
 
     assert response.status_code == 200
-    assert "message" in response.json()
-    assert response.json()["message"] == "Login Successful" 
+    assert response.json()["message"] == "Login Successful"
 
 # test case for Invalid login
 
 def test_login_invalid_password():
-    payload = {
+
+    # Create user first
+    client.post("/api/v1/users/create", json={
+        "name": "Jay Test",
+        "email": "jay@test.com",
+        "password": "Jay@12345"
+    })
+
+    # Try wrong password
+    response = client.post("/api/v1/users/login", json={
         "email": "jay@test.com",
         "password": "Wrong@123"
-    }
+    })
 
-    response = client.post("/users/login", json=payload)
-
-    assert response.status_code == 404
-    assert response.json()["detail"] == "Invalid password and email"
+    assert response.status_code == 401
+    assert response.json()["detail"] == "Invalid password"
