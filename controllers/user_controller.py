3f7548fb8f26736aafd0b01c4a560772a02eed33
agrawaljay12 <agrawaljay247@gmail.com>
2026-01-23 18:56:49 +0530

    refactor: update response handling in user creation and login functions

--- controllers/user_controller.py
+++ controllers/user_controller.py
@@ -74,16 +74,10 @@ async def create_user(request:Request):
         result = user_collection.insert_one(user_data.model_dump())
 
         # if data is inserted successfully then return success message and user id
-        return response.create_success_response(
-            status=http_status.CREATED,
+        return response.created_response(
             message=message.USER_CREATED_SUCCESS,
             data={"user_id": str(result.inserted_id)}
         )
-        # return ({
-        #     "status_code": http_status.CREATED,
-        #     "message":"User created successfully",
-        #     "user_id": str(result.inserted_id)
-        # })
      
     # Re-raise validation errors
     except HTTPException as e:
@@ -146,20 +140,12 @@ async def login_user(request:Request):
                 "user": token_data
             }
         )  
-        # return ({
-        #     "message":"Login Successful",
-        #     "access_token": access_token,
-        #     "token_type": "bearer",
-        #     "user":token_data
-        # })
      
      # Re-raise validation errors
      except HTTPException as e:
         raise e
-
           
 # get all users function
-
 async def get_all_users() -> list[dict]:
     try:
         users = []
