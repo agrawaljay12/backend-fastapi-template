d0a8e5f907692e6368cddf9e9be3578ed7b9c825
agrawaljay12 <agrawaljay247@gmail.com>
2026-03-19 09:53:54 +0530

    integrate the file upload functionality  and implement the cors origin

---
+++ utils/file_upload.py
@@ -0,0 +1,58 @@
+from fastapi import UploadFile,HTTPException,status
+from typing import List
+import os
+import uuid
+
+upload_dir = "static/uploads"
+
+async def save_file(folder:str,file:UploadFile):
+    try:
+        # full path of upload file 
+        full_path = os.path.join(upload_dir,folder)
+
+        if not os.path.exists(full_path):
+            os.makedirs(full_path,exist_ok=True)
+
+        # extract the file extension using split method 
+        file_extension = file.filename.split(".")
+
+        # give the unique filename to each file with file extension
+        unique_filename = f"{uuid.uuid4()}.{file_extension}"
+
+        file_path = os.path.join(full_path,unique_filename)
+
+        with open (file_path,"wb") as f:
+
+            content = await file.read()
+            f.write(content)
+
+        file_location = f"static/uploads/{folder}/{unique_filename}"
+
+        return file_location
+
+
+
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=str(e)
+        )
+    
+async def save_files(files:List[UploadFile],folder:str)->List[str]:
+    try:
+        
+        files_Path =[]
+
+        for file in files:
+
+            file_path = await save_file(file,folder)
+
+            files_Path.append(file_path)
+
+        return files_Path
+    
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=str(e)
+        )
